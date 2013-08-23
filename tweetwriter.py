80e20d7a0d74f7adcfb93ae6b1087e8649c13fac
Scott Perry <github@numist.net>
2013-08-22 22:48:23 -0700

    Magic constant breakout, soft line wrap, different exception stuff

--- tweetwriter.py
+++ tweetwriter.py
@@ -17,8 +17,16 @@ import dateutil.parser
 import pytz
 from rauth import OAuth1Session
 
-os.environ['TZ'] = 'us/Pacific'
-time.tzset()
+config_file = 'config.json'
+line_width = 65
+serial_glob = '/dev/ttyUSB*'
+serial_baud_rate = 115200
+serial_allowed_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()-_+=[]?/,.<>;:\'" \n'
+time_zone = 'US/Pacific'
+wait_char = 0.1
+wait_newline = 2.2
+wait_poll = 60
+wait_serial_init = 5
 
 def readFile(filename):
     f = open(filename, 'r')
@@ -27,19 +35,17 @@ def readFile(filename):
     return result
 
 class Printer:
-    allowed_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()-_+=[]?/,.<>;:\'" \n\t'
-
     def type(self, output):
-        devs = glob.glob('/dev/ttyUSB*')
+        devs = glob.glob(serial_glob)
         if len(devs):
             for dev in devs:
                 syslog.syslog(syslog.LOG_INFO, "Printing to "+dev)
-                self.sprint(dev, 115200, output)
+                self.sprint(dev, serial_baud_rate, output)
         else:
             syslog.syslog(syslog.LOG_WARNING, "No output devices found")
 
     def typeTweet(self, tweet, created, name, place):
-        timestamp = dateutil.parser.parse(created).astimezone(pytz.timezone('US/Pacific'))
+        timestamp = dateutil.parser.parse(created).astimezone(pytz.timezone(time_zone))
         output = "@"+name+" at "+timestamp.strftime("%H:%M:%S %Z")
         if place is not None:
             output = output+" in "+place['full_name']
@@ -51,25 +57,26 @@ class Printer:
 
     def sprint(self, path, baud, payload):
         port = serial.Serial(path, baud)
-        time.sleep(5)
+        # The FTDI chip resets the Arduino on connect, wait for it to stabilise!
+        time.sleep(wait_serial_init)
         pos = 0
         for c in payload:
-            if c not in self.allowed_characters:
+            if c not in serial_allowed_characters:
                 print "replacing character '"+c+"' with '?'"
                 c = "?"
             port.write(c)
             if c == '\n':
-                sleeptime = 2.0 * float(pos) / 65.0 + 0.2
+                sleeptime = float(wait_newline) * float(pos) / float(line_width) + 0.2
                 pos = 0
                 time.sleep(sleeptime)
             else:
                 pos = pos + 1
-                time.sleep(0.1)
-                """ Dumb line wrapping for now """
-                if pos >= 65:
-                    syslog.syslog(syslog.LOG_WARNING, "Force wrapping")
+                time.sleep(wait_char)
+                """ Dumb line wrapping """
+                if pos >= line_width:
+                    syslog.syslog(syslog.LOG_WARNING, "Force wrapping!")
                     port.write('\n')
-                    time.sleep(2.2)
+                    time.sleep(wait_newline)
                     pos = 0
 
 class Tweeter:
@@ -77,7 +84,7 @@ class Tweeter:
     latest = None
     def __init__(self, id=None):
         if self.session is None:
-            config = json.loads(readFile('config.json'))
+            config = json.loads(readFile(config_file))
             self.session = OAuth1Session(config['consumer_key'],
                                          config['consumer_secret'],
                                          config['access_token'],
@@ -100,6 +107,21 @@ class Tweeter:
         else:
             syslog.syslog(syslog.LOG_INFO, "No new tweets since " + self.latest)
         return []
+
+def softWrap(text, cols=80):
+    tokens = text.split(" ")
+    pos = 0
+    text = ""
+    for token in tokens:
+        if pos + 1 + len(token) >= cols:
+            text = text + "\n"
+            pos = 0
+        elif text != "":
+            text = text + " "
+            pos = pos + 1
+        text = text + token
+        pos = pos + len(token)
+    return text
         
 def printTweets(tweets):
     tweets.reverse()
@@ -108,28 +130,40 @@ def printTweets(tweets):
 
         """ NO MORE RETWEETS JESUS """
         if tweet['text'].startswith('RT @'):
-            print "Skipping RT by "+tweet['user']['screen_name']+"("+tweet['id_str']+")"
+            print "Skipping RT by "+tweet['user']['screen_name']+" ("+tweet['id_str']+")"
             continue
 
         """ CURLY QUOTES ARE THE DEVIL """
-        tweet['text'] = tweet['text'].replace('“'.decode('utf-8'), '"').replace('”'.decode('utf-8'), '"')
+        text = tweet['text'].replace('“'.decode('utf-8'), '"').replace('”'.decode('utf-8'), '"')
         """ SO ARE ELLIPSES """
-        tweet['text'] = tweet['text'].replace('…'.decode('utf-8'), '...')
+        text = text.replace('…'.decode('utf-8'), '...')
+        text = text.replace('\t', '    ')
 
-        Printer().typeTweet(h.unescape(tweet['text']), tweet['created_at'], tweet['user']['screen_name'], tweet['place'])
+        """ &amp -> &, etc """
+        text = h.unescape(text)
 
-tweeter = Tweeter()
+        text = softWrap(text, line_width)
+
+        Printer().typeTweet(text, tweet['created_at'], tweet['user']['screen_name'], tweet['place'])
+
+# Initialization
+os.environ['TZ'] = time_zone
+time.tzset()
+tweeter = Tweeter('370776114576113665')
 date = datetime.date.today()
 print "Most recent id: "+tweeter.latest
 
+# Runloop
 while 1:
     try:
         tweets = tweeter.fetch()
         printTweets(tweets)
-    except Exception as e:
-        print "Fetch error: "+e
+    except Exception:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+        print ''.join('!! ' + line for line in lines)
     
-    time.sleep(60)
+    time.sleep(wait_poll)
     newdate = datetime.date.today()
     if date.day != newdate.day:
         print "Day changed to "+date.strftime("%A %B %d, %Y")
