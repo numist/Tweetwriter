763182b2a0728691acdba6138406899dc26d1e83
Scott Perry <github@numist.net>
2013-08-22 18:00:54 -0700

    Various bug fixes and improvements (like disabling RTs and fixing curly quotes)

--- tweetwriter.py
+++ tweetwriter.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import serial
 import time
 import glob
 import json
+import HTMLParser
+import syslog
+import random
 
 from rauth import OAuth1Session
 
@@ -14,36 +18,41 @@ def readFile(filename):
     return result
 
 class Printer:
-    allowed_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()-_+=[]?/,.<>;:\'"'
+    allowed_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()-_+=[]?/,.<>;:\'" \n\t'
     def type(self, tweet, time, name):
-
-        print "@"+name+" tweeted: \""+tweet+"\" at "+time
+        print name + " " + time + " " + tweet
+        syslog.syslog(syslog.LOG_INFO, "Tweet by @"+name+" from "+time)
 
         devs = glob.glob('/dev/ttyUSB*')
         if len(devs):
             for dev in devs:
-                print "Pringing to "+dev
-                self.sprint(dev, 115200, "@"+name+": \""+tweet+"\"")
+                syslog.syslog(syslog.LOG_INFO, "Printing to "+dev)
+                self.sprint(dev, 115200, "@"+name+":\n"+tweet+"\n\n\n")
         else:
-            print "No output devices found"
+            syslog.syslog(syslog.LOG_WARNING, "No output devices found")
 
     def sprint(self, path, baud, payload):
         port = serial.Serial(path, baud)
+        time.sleep(5)
         pos = 0
         for c in payload:
             if c not in self.allowed_characters:
+                print "replacing character '"+c+"' with '?'"
                 c = "?"
             port.write(c)
             if c == '\n':
+                sleeptime = 2.0 * float(pos) / 65.0 + 0.2
                 pos = 0
-                time.sleep(2)
+                print "sleeping for "+str(sleeptime).rstrip('0')+" seconds"
+                time.sleep(sleeptime)
             else:
                 pos = pos + 1
                 time.sleep(0.1)
                 """ Dumb line wrapping for now """
-                if pos > 80:
+                if pos >= 65:
+                    syslog.syslog(syslog.LOG_WARNING, "Force wrapping")
                     port.write('\n')
-                    time.sleep(2)
+                    time.sleep(2.2)
                     pos = 0
 
 class Tweeter:
@@ -60,7 +69,7 @@ class Tweeter:
             self.fetch()
         else:
             self.latest = id
-            
+
     def fetch(self):
         params = {'q': '@Square', 'result_type': 'recent'}
         if self.latest is not None:
@@ -69,20 +78,29 @@ class Tweeter:
         if result['statuses'] and len(result['statuses']):
             tweets = result['statuses']
             self.latest = tweets[0]['id_str']
-            print "Tweeter: latest tweet id is " + self.latest
+            syslog.syslog(syslog.LOG_INFO, "Latest tweet id is " + self.latest)
             return tweets
         else:
-            print "Tweeter: no new tweets"
+            syslog.syslog(syslog.LOG_INFO, "No new tweets since " + self.latest)
         return []
         
 def printTweets(tweets):
     tweets.reverse()
+    h = HTMLParser.HTMLParser()
     for tweet in tweets:
-        Printer().type(tweet['text'], tweet['created_at'], tweet['user']['screen_name'])
+        """ NO MORE RETWEETS JESUS """
+        if tweet['text'].startswith('RT @'):
+            print "Skipping RT by "+tweet['user']['screen_name']
+            continue
 
-tweeter = Tweeter()
-tweets = tweeter.fetch()
-printTweets(tweets)
-print "sleeeeepy"
-tweeter.fetch()
-printTweets(tweets)
+        """ CURLY QUOTES ARE THE DEVIL """
+        tweet['text'] = tweet['text'].replace('“'.decode('utf-8'), '"').replace('”'.decode('utf-8'), '"')
+
+        Printer().type(h.unescape(tweet['text']), tweet['created_at'], tweet['user']['screen_name'])
+
+tweeter = Tweeter('370700607075524609')
+
+while 1:
+    tweets = tweeter.fetch()
+    printTweets(tweets)
+    time.sleep(60)
